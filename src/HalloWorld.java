46c2367391bad2424ad66ac89728c05b4a89cea7
tmarecik <57837773+tmarecik@users.noreply.github.com>
2019-11-16 16:32:37 +0100

    comit testowy

--- src/HalloWorld.java
+++ src/HalloWorld.java
@@ -2,6 +2,10 @@ public class HalloWorld {
     public static void main(String[] args) {
 
         System.out.println("HalloWorld");
+        System.out.println("HalloWorld1");
+        System.out.println("HalloWorld2");
+        
+        
     }
 
 
